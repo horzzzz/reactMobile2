6a9cc379fbf0641a2ed337d4a3bcc508c54f663b
horzzzz <tyuryuchkin@list.ru>
2024-03-12 20:36:40 +0200

    экран добавления работает, но нужно еще довести до ума

--- src/Screens/NotificationAddScreen.tsx
+++ src/Screens/NotificationAddScreen.tsx
@@ -1,14 +1,37 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-
-
+import useReminderStore from '../Store/store';
+import { useState } from 'react';
+import { Button, TextInput } from 'react-native';
 
 const NotificationAddScreen: React.FC = () => {
+  const [newReminder, setNewReminder] = useState('');
+  const { reminders, addReminder } = useReminderStore();
+  
+  const handleAddReminder = () => {
+    if (newReminder.trim() !== '') {
+      addReminder(newReminder);
+      setNewReminder('');
+    }
+  };
+
   return (
     <View>
-      <Text>Hello world!!!!</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
+        value={newReminder}
+        onChangeText={(text) => setNewReminder(text)}
+      />
+      <Button title="Add Reminder" onPress={handleAddReminder} />
+
+      <Text>Reminders:</Text>
+      {reminders.map((reminder, index) => (
+        <Text key={index}>{reminder}</Text>
+      ))}
     </View>
   );
 };
+
+
 
 export default NotificationAddScreen;
--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,11 +1,11 @@
-import create, { State } from 'zustand';
+import  {create SetState } from 'zustand';
 
-interface ReminderStore extends State {
+interface ReminderStore {
   reminders: string[];
   addReminder: (reminder: string) => void;
 }
 
-const useReminderStore = create<ReminderStore>((set) => ({
+const useReminderStore = create<ReminderStore>((set: SetState<ReminderStore>) => ({
   reminders: [],
   addReminder: (reminder) => set((state) => ({ reminders: [...state.reminders, reminder] })),
 }));
